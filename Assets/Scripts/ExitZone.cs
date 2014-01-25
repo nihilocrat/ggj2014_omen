a2bf6ec41bf26e406db8fce9a8a3fe005ab351e0
Kenny B <kenneth.backus@gmail.com>
2014-01-24 22:36:40 -0500

    Added "exit zone" to advance levels

---
+++ Assets/Scripts/ExitZone.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitZone : MonoBehaviour
+{
+	public string nextLevelName;
+
+	void OnTriggerEnter(Collider other)
+	{
+		other.gameObject.SendMessage("OnPlayerWin", nextLevelName);
+	}
+}
--- Assets/Scripts/PlayerDude.cs
+++ Assets/Scripts/PlayerDude.cs
@@ -7,7 +7,13 @@ public class PlayerDude : MonoBehaviour
 
 	void Start ()
 	{
-	
+		if(lastCheckPoint == null)
+		{
+			lastCheckPoint = GameObject.FindGameObjectWithTag("Respawn").transform;
+		}
+
+		// send me to the spawnpoint
+		OnPlayerDeath(null);
 	}
 
 	void Update ()
@@ -20,9 +26,20 @@ public class PlayerDude : MonoBehaviour
 
 	void OnPlayerDeath(GameObject killer)
 	{
-		killer.SendMessage("OnKilledPlayer");
+		if(killer != null)
+		{
+			killer.SendMessage("OnKilledPlayer");
+		}
 
 		transform.position = lastCheckPoint.position;
 		transform.rotation = lastCheckPoint.rotation;
+	}
+
+	void OnPlayerWin(string nextLevelName)
+	{
+		if(string.IsNullOrEmpty(nextLevelName))
+		{
+			Application.LoadLevel(Application.loadedLevel + 1);
+		}
 	}
 }
