0a295c9488500b78aecf9bd4c8e169e7c04518e3
Kenny B <kenneth.backus@gmail.com>
2014-01-25 17:23:29 -0500

    Password keypad

--- Assets/Keypad.cs
+++ Assets/Keypad.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class Keypad : MonoBehaviour
 {	
+	public bool numericInput = true;
+	public string[] possiblePasswords;
+	
 	private string currentCode = ":) :) :)";
 	private string targetCode = "1234";
 	private bool showKeypad = false;
@@ -12,6 +15,10 @@ public class Keypad : MonoBehaviour
 	void Start()
 	{
 		targetCode = CreateCode();
+		if(!numericInput)
+		{
+			currentCode = "";
+		}
 	}
 
 	void OnGrabbed(GameObject grabber)
@@ -41,39 +48,52 @@ public class Keypad : MonoBehaviour
 		}
 		else
 		{
-			for(int i=0; i < 10; i++)
+			if(numericInput)
 			{
-				int num = i + 1;
-				int col = i % 3;
-				int row = Mathf.FloorToInt(i / 3);
-				
-				if(i != 9)
+				for(int i=0; i < 10; i++)
 				{
-					if(GUI.Button(new Rect(origin.x + col * 100f, origin.y + row * 100f, buttonsize.x, buttonsize.y), num.ToString()))
+					int num = i + 1;
+					int col = i % 3;
+					int row = Mathf.FloorToInt(i / 3);
+					
+					if(i != 9)
 					{
-						InputNumber(num);
+						if(GUI.Button(new Rect(origin.x + col * 100f, origin.y + row * 100f, buttonsize.x, buttonsize.y), num.ToString()))
+						{
+							InputNumber(num);
+						}
+					}
+					// special case for "0"
+					else
+					{
+						col = 1;
+						row = 3;
+						
+						if(GUI.Button(new Rect(origin.x + col * 100f, origin.y + row * 100f, buttonsize.x, buttonsize.y), "0"))
+						{
+							InputNumber(0);
+						}
+						
+						// exit key
+						col += 1;
+						if(GUI.Button(new Rect(origin.x + col * 100f, origin.y + row * 100f, buttonsize.x, buttonsize.y), "X"))
+						{
+							currentCode = ":) :) :)";
+							CloseKeypad();
+						}
+						
+						break;
 					}
 				}
-				// special case for "0"
-				else
+			}
+			else
+			{
+				currentCode = GUI.TextField(new Rect(origin.x, origin.y + 40f, size.x, 20f), currentCode, 40);
+				
+				if(GUI.Button(new Rect(origin.x, origin.y + 80f, buttonsize.x, buttonsize.y), "ENTER")
+				   || Input.GetKeyDown(KeyCode.Return))
 				{
-					col = 1;
-					row = 3;
-					
-					if(GUI.Button(new Rect(origin.x + col * 100f, origin.y + row * 100f, buttonsize.x, buttonsize.y), "0"))
-					{
-						InputNumber(0);
-					}
-
-					// exit key
-					col += 1;
-					if(GUI.Button(new Rect(origin.x + col * 100f, origin.y + row * 100f, buttonsize.x, buttonsize.y), "X"))
-					{
-						currentCode = ":) :) :)";
-						CloseKeypad();
-					}
-
-					break;
+					ValidateCode();
 				}
 			}
 		}
@@ -92,8 +112,16 @@ public class Keypad : MonoBehaviour
 
 	string CreateCode()
 	{
-		int codeNum = Random.Range(1001, 9999);
-		return codeNum.ToString();
+		if(numericInput)
+		{
+			int codeNum = Random.Range(1001, 9999);
+			return codeNum.ToString();
+		}
+		else
+		{
+			int index = Random.Range(0, possiblePasswords.Length);
+			return possiblePasswords[index];
+		}
 	}
 
 	void InputNumber(int num)
@@ -108,17 +136,22 @@ public class Keypad : MonoBehaviour
 
 		if(currentCode.Length >= targetCode.Length)
 		{
-			if(currentCode == targetCode)
-			{
-				// success!
-				Debug.Log("KEYPAD: success!");
-				OnKeypadSuccess();
-			}
-			else
-			{
-				Debug.Log("KEYPAD: failure!");
-				currentCode += " INVALID CODE! >:(";
-			}
+			ValidateCode();
+		}
+	}
+
+	void ValidateCode()
+	{
+		if(currentCode == targetCode)
+		{
+			// success!
+			Debug.Log("KEYPAD: success!");
+			OnKeypadSuccess();
+		}
+		else
+		{
+			Debug.Log("KEYPAD: failure!");
+			currentCode = "INVALID CODE! >:(";
 		}
 	}
 
