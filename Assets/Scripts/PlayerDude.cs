994e42f9e8b420c3179960cfd81f25bc1a30f538
Kenny B <kenneth.backus@gmail.com>
2014-01-25 12:05:34 -0500

    Level 1 fixes

--- Assets/Scripts/PlayerDude.cs
+++ Assets/Scripts/PlayerDude.cs
@@ -75,4 +75,27 @@ public class PlayerDude : MonoBehaviour
 		grabbed.rigidbody.useGravity = false;
 		grabbed.rigidbody.isKinematic = true;
 	}
+
+	// this script pushes all rigidbodies that the character touches
+	private  float pushPower = 2.0f;
+	void OnControllerColliderHit (ControllerColliderHit hit)
+	{
+		var body = hit.collider.attachedRigidbody;
+		// no rigidbody
+		if (body == null || body.isKinematic)
+			return;
+		
+		// We dont want to push objects below us
+		if (hit.moveDirection.y < -0.3f) 
+			return;
+		
+		// Calculate push direction from move direction, 
+		// we only push objects to the sides never up and down
+		var pushDir = new Vector3(hit.moveDirection.x, 0f, hit.moveDirection.z);
+		// If you know how fast your character is trying to move,
+		// then you can also multiply the push velocity by that.
+		
+		// Apply the push
+		body.velocity = pushDir * pushPower;
+	}
 }
