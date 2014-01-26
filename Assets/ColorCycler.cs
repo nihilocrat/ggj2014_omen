97ee89bd298cfb245cfe63b328e79f876a392ca6
Kenny B <kenneth.backus@gmail.com>
2014-01-25 21:41:01 -0500

    Added new font and fun color cycling

---
+++ Assets/ColorCycler.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorCycler : MonoBehaviour
+{
+	public Material targetMaterial;
+	public Color beginColor;
+	public Color endColor;
+	public string colorPropertyName = "_Main";
+	public float time = 1.0f;
+
+	private Color original;
+	private float t = 0f;
+	private float sign = 1f;
+
+	void Awake()
+	{
+		original = targetMaterial.GetColor(colorPropertyName);
+	}
+
+	void OnDestroy()
+	{
+		targetMaterial.SetColor(colorPropertyName, original);
+	}
+
+	void Update()
+	{
+		t += (Time.deltaTime / time) * sign;
+		if(t > 1f || t < 0f)
+		{
+			sign *= -1f;
+		}
+		/*
+		Color newColor = new Color(
+			Mathf.Lerp(beginColor.r, endColor.r, t),
+			Mathf.Lerp(beginColor.g, endColor.g, t),
+			Mathf.Lerp(beginColor.b, endColor.b, t),
+			1.0f);
+		*/
+		targetMaterial.SetColor(colorPropertyName, Color.Lerp(beginColor, endColor, t));
+	}
+}
